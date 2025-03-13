6c7036b11361a84b421730b32eaad22314e8ec35
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-03-14 00:37:00 +0500

    update

--- Express.js/Lessons/Lesson-02/main.js
+++ Express.js/Lessons/Lesson-02/main.js
@@ -1,10 +1,38 @@
 const express = require('express')
+const blog = require('./routes/blogs')
+const shop = require('./routes/shop')
+
+
 const app = express()
 const port = 3000
 
+app.use(express.static("public"))
+app.use("/blog", blog)
+app.use("/shop", shop)
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    console.log("It is Get Request")
+    res.send('Hello World!')
+}).post('/', (req, res) => {
+    console.log("It is Post Request")
+    res.send('Hello World Post')
 })
+
+
+app.put('/', (req, res) => {
+    console.log("It is Put Request")
+    res.send('Hello World Put')
+})
+
+app.get('/index', (req, res) => {
+    console.log("It is Index Page")
+    res.sendFile('tamplates/index.html', {root:__dirname})
+})
+
+app.get('/api', (req, res) => {
+    res.json({a:1, b:2, c:3, name:["areeb", "Ali"]})
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
