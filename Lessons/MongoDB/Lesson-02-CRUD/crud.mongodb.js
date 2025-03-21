e6e9edabccafb5a4f38f660de548095d3804f615
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-03-21 23:10:09 +0500

    Update crud.mongodb.js

--- Lessons/MongoDB/Lesson-02-CRUD/crud.mongodb.js
+++ Lessons/MongoDB/Lesson-02-CRUD/crud.mongodb.js
@@ -1,14 +1,31 @@
-use("crudDb")
+// CRUD Operations
+// use("crudDb")
 
-db.createCollection("courses")
+// db.createCollection("courses")
 
-db.courses.insertOne({
-    name:"Web Development",
-    price:200,
-    assignments:12,
-    projects:5
-})
+// Create
 
-let a = db.courses.find({price:200})
+// db.courses.insertOne({
+//     name:"Web Development",
+//     price:200,
+//     assignments:12,
+//     projects:5
+// })
 
-console.log(a.toArray())
+
+// Read
+
+// let a = db.courses.find({price:200})
+
+// console.log(a.toArray())
+
+// Update
+
+// db.courses.updateOne({price:200}, {$set:{price:50}})
+
+// db.courses.updateMany({price:200}, {$set:{price:500}})
+
+
+// Delete
+
+// db.courses.deleteOne({price:50})
