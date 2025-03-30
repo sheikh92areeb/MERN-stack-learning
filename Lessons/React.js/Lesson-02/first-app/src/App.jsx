87b42e36640f26b9703a937bc80856e13eb4428b
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-03-30 22:01:08 +0500

    update

--- Lessons/React.js/Lesson-02/first-app/src/App.jsx
+++ Lessons/React.js/Lesson-02/first-app/src/App.jsx
@@ -1,9 +1,19 @@
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+import Card from "./components/Card"
 
 function App() {
 
   return (
     <>
-      <div>Areeb</div>
+      <Navbar />
+      <div className="cards">
+        <Card title="card 1" description="card 1 desc" />
+        <Card title="card 2" description="card 2 desc" />
+        <Card title="card 3" description="card 3 desc" />
+        <Card title="card 4" description="card 4 desc" />
+      </div>
+      <Footer />
     </>
   )
 }
---
+++ Lessons/React.js/Lesson-02/first-app/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Footer.css"
+const Footer = () => {
+  return (
+    <div className='footer'>
+      Copyright &copy; Areeb Sheikh | All rights reserved
+    </div>
+  )
+}
+
+export default Footer
