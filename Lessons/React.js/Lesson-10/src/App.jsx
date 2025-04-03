d280fa78e40f6f60a54cff149fbc53ab68e8633d
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-04-03 23:54:34 +0500

    update

--- Lessons/React.js/Lesson-10/src/App.jsx
+++ Lessons/React.js/Lesson-10/src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
+import { useState, useMemo } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+const nums = new Array(30_000_000).fill(0).map((_, i)=> {
+  return {
+    index : i,
+    isMagical: i === 29_000_000
+  }
+})
+
 function App() {
   const [count, setCount] = useState(0)
+  const [numbers, setNumbers] = useState(nums)
+
+  // const magical = numbers.find((item)=> item.isMagical === true)
+  const magical = useMemo(() => numbers.find((item)=> item.isMagical === true), [numbers])
 
   return (
     <>
       <div>
+        <span>Magical Number is {magical.index}</span>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -18,7 +31,19 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => {          
+          setCount((count) => count + 1)
+          if (count == 10) {
+            setNumbers(
+              new Array(30_000).fill(0).map((_, i)=> {
+                return {
+                  index : i,
+                  isMagical: i === 900
+                }
+              })              
+            )
+          }
+          }}>
           count is {count}
         </button>
         <p>
